9af868d9316a8d73baf3f0c90c79acd9b7f5d488
otto <you@example.com>
2026-03-02 10:43:39 +0200

    post

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
+
+Route::apiResource('books', BooksController::class);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/test', function () {
+    return array("otto"=>"sula");
+});
+
+
